5cf688fba6e619b14fc35029f13e1c2d4597935f
BerlianoSurya <berlianosurya@gmail.com>
2025-03-01 10:49:00 +0700

    first commit

---
+++ components/categories.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import Link from "next/link"
+import { Separator } from "@radix-ui/react-separator"
+import {
+  BanknoteIcon,
+  BikeIcon,
+  CpuIcon,
+  FlaskConical,
+  TvIcon,
+} from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+
+const categories = [
+  { label: "bussiness", icon: BanknoteIcon },
+  { label: "entertainment", icon: BanknoteIcon },
+  { label: "science", icon: FlaskConical },
+  { label: "technology", icon: CpuIcon },
+  { label: "sports", icon: BikeIcon },
+]
+
+export const Categories = () => {
+  return (
+    <NavigationMenu className="max-w-[95vw] flex items-start justify-start">
+      <NavigationMenuList>
+        {categories.map((category, index) => (
+          <NavigationMenuItem key={category.label}>
+            <Link href={`/category/${category.label}`} passHref legacyBehavior>
+              <NavigationMenuLink
+                className={cn(
+                  navigationMenuTriggerStyle(),
+                  "px-2 transition-all hover:bg-transparent hover:text-orange-800"
+                )}
+              >
+                <div className="flex items-center justify-between gap-2 capitalize">
+                  {<category.icon className="size-4" />}
+                  <span className="text-sm">{category.label}</span>
+                  {index === categories.length - 1 ? null : (
+                    <Separator
+                      orientation="vertical"
+                      className="ml-4 h-8 rotate-45 w-0.5 bg-orange-700 text-orange-700"
+                    />
+                  )}
+                </div>
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        ))}
+      </NavigationMenuList>
+    </NavigationMenu>
+  )
+}
---
+++ components/site-header.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import Link from "next/link"
+
+import { siteConfig, SiteConfig } from "@/config/site"
+import { Categories } from "@/components/categories"
+
+import { ScrollArea, ScrollBar } from "./ui/scroll-area"
+import { H1 } from "./ui/typography"
+
+export function SiteHeader() {
+  return (
+    <header className="flex w-full flex-col items-center justify-between md:flex-row md:border-b pt-8 pb-4 px-8 border-b">
+      <H1 className="uppercase text-orange-700 lg:text-6xl">
+        <Link href={"/"}>{siteConfig.name}</Link>
+      </H1>
+      <ScrollArea className="whitespace-nowrap">
+        <Categories />
+        <ScrollBar orientation="horizontal" className="md:hidden" />
+      </ScrollArea>
+    </header>
+  )
+}
